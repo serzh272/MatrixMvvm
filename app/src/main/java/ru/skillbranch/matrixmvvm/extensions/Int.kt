f3132c25b4444ffa8d7be9118e156530220c07cb
serzh272 <serzh272@mail.ru>
2021-01-29 17:46:19 +0900

    Add Extensions

---
+++ app/src/main/java/ru/skillbranch/matrixmvvm/extensions/Int.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.matrixmvvm.extensions
+
+import ru.serzh272.matrix.Fraction
+
+@ExperimentalUnsignedTypes
+operator fun Int.plus(fr: Fraction): Fraction {
+    return fr + this
+}
+
+@ExperimentalUnsignedTypes
+operator fun Int.minus(fr: Fraction): Fraction {
+    return (-fr) + this
+}
+
+@ExperimentalUnsignedTypes
+operator fun Int.times(fr: Fraction): Fraction {
+    return fr * this
+}
+
+@ExperimentalUnsignedTypes
+operator fun Int.div(fr: Fraction): Fraction {
+    return this * (fr.invert())
+}
